9900031058c9c6082e6d32a6fb9f5c74173cc553
liangpeili <liangpeili@foxmail.com>
2019-12-28 12:31:45 +0800

    add logout button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,25 @@
 <template>
   <el-row>
-    <el-col :span="24">
+    <el-col :span="22">
       <div class="head-wrap">GNY Wallet</div>
+    </el-col>
+    <el-col :span="2">
+      <el-button @click="logout" type="info">Logout</el-button>
     </el-col>
   </el-row>
 </template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.dispatch('setToken', null);
+      this.$store.dispatch('setUser', null);
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
 
 <style scoped>
 .head-wrap {
