f3b3024bb4bb1c47f77952700a9fc540371a92c4
liangpeili <liangpeili@foxmail.com>
2019-12-31 16:04:45 +0800

    feature: add transaction history

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,35 +1,46 @@
 <template>
   <div>
-    <el-card class="box-card">
+    <el-card class="box-card1">
       <div slot="header" class="clearfix">
         <span>Account Info</span>
       </div>
       <p v-if="user.username">{{ user.username }}</p>
       <p>Address: {{ user.address }}</p>
-      <p>Balance: {{ user.balance }}</p>
+      <p>Balance: {{ user.balance / 1e8 }} GNY</p>
       <!--<p>Public key: {{ user.publicKey }}</p>-->
     </el-card>
-    <el-card class="box-card">
+    <el-card class="box-card2">
       <div slot="header" class="clearfix">
         <span>Transaction History</span>
       </div>
-      <div v-for="o in 4" :key="o" class="text item">
-        {{ '列表内容 ' + o }}
-      </div>
+      <el-table :data="transactions" style="width: 100%">
+        <el-table-column prop="id" label="ID" width="180"></el-table-column>
+        <el-table-column prop="type" label="Type" width="80"></el-table-column>
+        <el-table-column prop="args" label="Args"></el-table-column>
+        <el-table-column prop="height" label="height" width="80"></el-table-column>
+        <el-table-column prop="message" label="Message"></el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
 
 <script scoped>
+import * as gnyClient from "@gny/client";
+const connection = new gnyClient.Connection("192.168.1.252", 4096, "testnet");
+
 export default {
   data() {
     return {
-      user: {}
+      user: {},
+      transactions: []
     };
   },
-  mounted() {
+  async mounted() {
     this.user = this.$store.state.user;
-    console.log(this.$store.state.user);
+    this.transactions = (await connection.api.Transaction.getTransactions({
+      senderId: this.user.address
+    })).transactions;
+    console.log(this.transactions);
   }
 };
 </script>
@@ -43,10 +54,17 @@ export default {
   padding: 18px 18px;
 }
 
-.box-card {
+.box-card1 {
   margin: 18px 18px;
   padding: 18px 18px;
   width: 480px;
+  float: left;
+}
+
+.box-card2 {
+  margin: 18px 18px;
+  padding: 18px 18px;
+  width: 100%;
   float: left;
 }
 </style>
