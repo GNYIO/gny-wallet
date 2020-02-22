b07d5ea5889cddf47dae579f27af7423573763af
Matthias Knopp <matthias-knopp@gmx.net>
2020-02-22 16:52:41 +0100

    chore: add normalize css and use english locale

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import App from './App.vue';
 import router from './router';
+import 'normalize.css'
 import 'vue-awesome/icons';
+import 'element-theme-chalk';
 import Icon from 'vue-awesome/components/Icon';
 import store from './store/store';
-import 'element-theme-chalk';
+import locale from 'element-ui/lib/locale/lang/en';
 
 Vue.config.productionTip = false;
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 Vue.component('icon', Icon);
 
 new Vue({
