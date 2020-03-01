5083b02616abf7052ee476609044fbffd43d3e1c
liangpeili <liangpeili@foxmail.com>
2020-03-01 19:53:19 +0800

    refractor login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,35 +1,29 @@
 <template>
   <el-main>
     <img alt="GNY logo" src="../assets/logo.png" />
-    <br />
 
-    <el-input v-model="newAccount" v-if="!!newAccount"></el-input>
-    <div align="center" v-if="!!newAccount">
-      <el-alert
-        title="Please save your passphrase to a safe place!"
-        type="error"
-        :closable="false"
-        center
-      >
-      </el-alert>
-    </div>
-    <br />
-    <el-input
-      placeholder="Enter your passphrase"
-      v-model="passphrase"
-      show-password
-    ></el-input>
-    <br />
-    <br />
+    <el-input v-model="newAccount" v-if="newAccount"></el-input>
 
-    <el-button type="info" @click="login">Login</el-button>
-    <el-button type="info" @click="register">New Account</el-button>
+    <el-alert
+      title="Please save your passphrase to a safe place!"
+      type="error"
+      :closable="false"
+      center
+      v-if="newAccount"
+    ></el-alert>
+
+    <el-input placeholder="Enter your passphrase" v-model="passphrase" show-password></el-input>
+
+    <el-row>
+      <el-button type="info" @click="login">Login</el-button>
+      <el-button type="info" @click="register">New Account</el-button>
+    </el-row>
   </el-main>
 </template>
 
 <script>
-import * as Mnemonic from 'bitcore-mnemonic';
-import * as Cookie from 'tiny-cookie'
+import * as Mnemonic from "bitcore-mnemonic";
+import * as Cookie from "tiny-cookie";
 
 export function generateSecret() {
   return new Mnemonic(Mnemonic.Words.ENGLISH).toString();
@@ -38,8 +32,8 @@ export function generateSecret() {
 export default {
   data() {
     return {
-      passphrase: '',
-      newAccount: '',
+      passphrase: "",
+      newAccount: ""
     };
   },
   methods: {
@@ -47,40 +41,48 @@ export default {
       try {
         this.newAccount = generateSecret();
       } catch (error) {
-        console.log(error);
+        console.log(`Register error: ${error}`);
       }
     },
     async login() {
       try {
-        Cookie.set('bip39', this.passphrase);
-
-        this.$store.dispatch('setPassphrase', this.passphrase);
-        this.$store.dispatch('setToken', this.passphrase);
-        await this.$store.dispatch('refreshAccounts');
-        this.$router.push('/home');
+        Cookie.set("bip39", this.passphrase);
+        await this.$store.dispatch("setPassphrase", this.passphrase);
+        await this.$store.dispatch("setToken", this.passphrase);
+        await this.$store.dispatch("refreshAccounts");
+        this.$router.push("/home");
         location.reload();
       } catch (error) {
-        console.log('in login', error);
+        console.log(`Login error: ${error}`);
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
 .el-main {
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 img {
   width: 300px;
+  margin: 100px 0;
 }
 
 .el-input {
   width: 500px;
+  margin: 15px 0;
 }
 
 .el-alert {
   width: 500px;
+}
+
+.el-input {
+  display: block-inline;
 }
 </style>
