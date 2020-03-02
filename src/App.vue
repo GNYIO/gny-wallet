98c1ef4ca29d15c41bd8ddf2f4e4736a408aa756
liangpeili <liangpeili@foxmail.com>
2020-03-02 16:15:47 +0800

    remove useless code

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,3 @@ export default {
   name: 'app',
 };
 </script>
-
-<style>
-.header {
-  background-color: #0a68ab;
-  color: #fff;
-  line-height: 60px;
-}
-.app {
-  font-family: 'Courier New', Courier, monospace;
-}
-</style>
