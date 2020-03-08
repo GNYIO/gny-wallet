7885a50266f632d8d8cbdab5b564b7f3220edade
Matthias Knopp <matthias-knopp@gmx.net>
2020-03-08 10:43:19 +0100

    chore: split vuex store into separate files

---
+++ src/store/state.js
@@ -0,0 +1,17 @@
+
+export const state = {
+  passphrase: null,
+  user: null,
+  delegate: null,
+  isLoggedIn: false,
+  token: '',
+  latestBlock: {},
+  myVoters: [],
+  allDelegateNames: [],
+  isIssuer: false,
+  issuer: {},
+  assets: [],
+  transactions: [],
+  whoIVotedFor: [],
+};
+
