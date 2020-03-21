1ba0cd440c25209d35bcd20e62a2ba03645d86f3
Matthias Knopp <matthias-knopp@gmx.net>
2020-03-22 00:38:29 +0100

    chore: activate normalize.css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import locale from 'element-ui/lib/locale/lang/en';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
+import 'normalize.css';
 
 Vue.config.productionTip = false;
 
