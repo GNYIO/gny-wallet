0278e3b9dd636f2227830943b4a5061860920a8e
Matthias Knopp <matthias-knopp@gmx.net>
2020-03-23 11:01:06 +0100

    chore: lint all files

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -22,46 +22,40 @@
 
     <el-container direction="horizontal">
       <div style="display:relative">
-      <el-aside>
-        <el-menu
-          class="el-menu-vertical"
-          active-text-color="#ffd04b"
-          router
-        >
-          <el-menu-item index="1" route="/home">
-            <i class="el-icon-s-home"></i>
-            <span> Home</span>
-          </el-menu-item>
+        <el-aside>
+          <el-menu class="el-menu-vertical" active-text-color="#ffd04b" router>
+            <el-menu-item index="1" route="/home">
+              <i class="el-icon-s-home"></i>
+              <span> Home</span>
+            </el-menu-item>
 
-          <el-menu-item index="2" route="/transfer">
-            <i class="el-icon-s-promotion"></i>
-            <span> Transfer</span>
-          </el-menu-item>
+            <el-menu-item index="2" route="/transfer">
+              <i class="el-icon-s-promotion"></i>
+              <span> Transfer</span>
+            </el-menu-item>
 
-          <el-menu-item index="3" route="/delegates">
-            <i class="el-icon-user-solid"></i>
-            <span> Delegates</span>
-          </el-menu-item>
+            <el-menu-item index="3" route="/delegates">
+              <i class="el-icon-user-solid"></i>
+              <span> Delegates</span>
+            </el-menu-item>
 
-          <el-menu-item index="4" route="/assets">
-            <i class="el-icon-s-order"></i>
-            <span> Assets</span>
-          </el-menu-item>
+            <el-menu-item index="4" route="/assets">
+              <i class="el-icon-s-order"></i>
+              <span> Assets</span>
+            </el-menu-item>
 
-          <el-menu-item index="5" route="/machinelearning">
-            <i class="el-icon-s-platform"></i>
-            <span> Machine Learning</span>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
+            <el-menu-item index="5" route="/machinelearning">
+              <i class="el-icon-s-platform"></i>
+              <span> Machine Learning</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
       </div>
 
       <el-main>
         <router-view></router-view>
       </el-main>
-
     </el-container>
-
   </el-container>
 </template>
 
@@ -98,10 +92,9 @@ export default {
   height: 100%;
   z-index: 100;
   border-right: solid 1px #e6e6e6;
-  margin:0px;
+  margin: 0px;
 }
 .el-aside > * {
   border-right: solid 0px;
 }
-
 </style>
