d0babe40847a0b15979dcec78428cc677367ef63
Matthias Knopp <matthias-knopp@gmx.net>
2020-06-27 13:25:41 +0200

    chore: add LockAccountComponent.vue #40

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,69 +18,13 @@
         v-bind:hasSecondPassphrase="hasSecondPassphrase"
       ></Set2ndPasswordComponent>
 
-      <el-col :span="12" v-if="user.lockHeight === '0'">
-        <el-card v-if="!positiveBalance">
-          <div>
-            <h3>You need 0.1 GNY to lock your account</h3>
-          </div>
-        </el-card>
-
-        <el-card v-if="positiveBalance">
-          <div slot="header">
-            Lock your account
-          </div>
-
-          <el-form
-            ref="lockAccountForm"
-            :model="lockAccountForm"
-            label-width="80px"
-            :rules="lockAccountFormRules"
-          >
-            <el-form-item label="height" prop="lockHeight">
-              <el-tooltip
-                effect="light"
-                content="Hide amount until lock height"
-                placement="top-start"
-              >
-                <el-input
-                  v-model="lockAccountForm.lockHeight"
-                  :disabled="isLocked"
-                ></el-input>
-              </el-tooltip>
-            </el-form-item>
-            <el-form-item label="amount" prop="lockAmount">
-              <el-tooltip
-                effect="light"
-                content="Amount to lock"
-                placement="top-start"
-              >
-                <el-input
-                  v-model="lockAccountForm.lockAmount"
-                  :disabled="isLocked"
-                ></el-input>
-              </el-tooltip>
-            </el-form-item>
-            <el-form-item>
-              <div style="float: left;">
-                <el-badge
-                  value="0.1 GNY"
-                  type="info"
-                  @mouseover.native="hideLockBadge = false"
-                  @mouseleave.native="hideLockBadge = true"
-                  :hidden="hideLockBadge"
-                >
-                  <el-button
-                    type="primary"
-                    @click="lockAccount"
-                    :disabled="isLocked"
-                    >Lock Account</el-button
-                  >
-                </el-badge>
-              </div>
-            </el-form-item>
-          </el-form>
-        </el-card>
-      </el-col>
+      <LockAccountComponent
+        v-bind:user="user"
+        v-bind:positiveBalance="positiveBalance"
+        v-bind:minLockHeight="minLockHeight"
+        v-bind:passphrase="passphrase"
+        v-bind:secondPassphrase="secondPassphrase"
+      ></LockAccountComponent>
 
       <el-col :span="12" v-if="user.lockHeight !== '0'">
         <el-card v-if="!positiveBalance">
@@ -131,6 +75,7 @@ import TransactionsPaged from './TransactionsPaged';
 import AccountInfoComponent from './Home/AccountInfoComponent';
 import SetUsernameComponent from './Home/SetUsernameComponent';
 import Set2ndPasswordComponent from './Home/Set2ndPasswordComponent';
+import LockAccountComponent from './Home/LockAccountComponent';
 
 import { BigNumber } from 'bignumber.js';
 
@@ -148,64 +93,11 @@ export default {
     AccountInfoComponent,
     SetUsernameComponent,
     Set2ndPasswordComponent,
+    LockAccountComponent,
   },
   data() {
-    const validateBlockHeight = (rule, value, callback) => {
-      try {
-        const minLockHeight = new BigNumber(this.minLockHeight);
-        if (minLockHeight.isLessThan(value)) {
-          callback();
-        } else {
-          callback(
-            new Error(`Lock height must be greater than ${this.minLockHeight}`),
-          );
-        }
-      } catch (err) {
-        callback(
-          new Error(`Lock height must be greater than ${this.minLockHeight}`),
-        );
-      }
-    };
-
     return {
-      hideLockBadge: true,
-
-      isLocked: false,
-
       placeholder: '',
-
-      lockAccountForm: {
-        lockHeight: '',
-        lockAmount: '',
-      },
-      lockAccountFormRules: {
-        lockHeight: [
-          {
-            required: true,
-            message: 'Please add a lockHeight',
-            trigger: 'change',
-          },
-          {
-            pattern: /^[1-9][0-9]*$/,
-            trigger: 'change',
-          },
-          {
-            validator: validateBlockHeight,
-            trigger: 'change',
-          },
-        ],
-        lockAmount: [
-          {
-            required: true,
-            message: 'Please add a lockAmount',
-            trigger: 'change',
-          },
-          {
-            pattern: /^[1-9][0-9]*$/,
-            trigger: 'change',
-          },
-        ],
-      },
 
       unlockAccountForm: {},
       hideUnlockAccountBadge: true,
@@ -221,33 +113,6 @@ export default {
     ]),
   },
   methods: {
-    async lockAccount() {
-      try {
-        await this.$refs['lockAccountForm'].validate();
-      } catch (err) {
-        console.log(err);
-        console.log('Validation for lockAccountForm failed');
-        return;
-      }
-
-      try {
-        const height = this.lockAccountForm.lockHeight;
-        const amount = new BigNumber(this.lockAccountForm.lockAmount)
-          .multipliedBy(1e8)
-          .toFixed();
-
-        const result = await connection.contract.Basic.lockAccount(
-          height,
-          amount,
-          this.passphrase,
-          this.secondPassphrase,
-        );
-        this.$message(result.transactionId);
-        this.isLocked = true;
-      } catch (err) {
-        console.log(err.message);
-      }
-    },
     async unlockAccount() {
       const currentHeight = new BigNumber(this.latestBlock.height);
       const lockHeight = new BigNumber(this.user.lockHeight);
