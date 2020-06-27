2ee929a78ce16bade07f8d8240b3e5460b5af2f7
Matthias Knopp <matthias-knopp@gmx.net>
2020-06-27 11:16:28 +0200

    chore: add SetUsernameComponent.vue #40

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,53 +2,12 @@
   <div>
     <el-row :gutter="20">
       <AccountInfoComponent v-bind:user="user"></AccountInfoComponent>
-
-      <el-col :span="12">
-        <el-card v-if="!positiveBalance">
-          <div>
-            <h3>You need 5 GNY to set your username</h3>
-          </div>
-        </el-card>
-
-        <el-card v-if="!user.username && positiveBalance">
-          <div slot="header">
-            <span>Set Username</span>
-          </div>
-
-          <el-form
-            ref="usernameForm"
-            :model="usernameForm"
-            label-width="80px"
-            :rules="usernameFormRules"
-          >
-            <el-form-item label="Usern." prop="username">
-              <el-input
-                v-model="usernameForm.username"
-                :disabled="hasUsername"
-              ></el-input>
-            </el-form-item>
-
-            <el-form-item>
-              <div style="float: left">
-                <el-badge
-                  value="5 GNY"
-                  type="info"
-                  @mouseover.native="hideSetUsernameBadge = false"
-                  @mouseleave.native="hideSetUsernameBadge = true"
-                  :hidden="hideSetUsernameBadge"
-                >
-                  <el-button
-                    type="primary"
-                    @click="setUsername"
-                    :disabled="hasUsername"
-                    >Set Username</el-button
-                  >
-                </el-badge>
-              </div>
-            </el-form-item>
-          </el-form>
-        </el-card>
-      </el-col>
+      <SetUsernameComponent
+        v-bind:user="user"
+        v-bind:positiveBalance="positiveBalance"
+        v-bind:passphrase="passphrase"
+        v-bind:secondPassphrase="secondPassphrase"
+      ></SetUsernameComponent>
     </el-row>
 
     <el-row :gutter="20">
@@ -216,6 +175,8 @@
 import { mapState, mapGetters } from 'vuex';
 import TransactionsPaged from './TransactionsPaged';
 import AccountInfoComponent from './Home/AccountInfoComponent';
+import SetUsernameComponent from './Home/SetUsernameComponent';
+
 import { BigNumber } from 'bignumber.js';
 
 import * as client from '@gny/client';
@@ -230,6 +191,7 @@ export default {
   components: {
     TransactionsPaged,
     AccountInfoComponent,
+    SetUsernameComponent,
   },
   data() {
     const validateBlockHeight = (rule, value, callback) => {
@@ -251,31 +213,12 @@ export default {
 
     return {
       hideLockBadge: true,
-      hideSetUsernameBadge: true,
       hideSetSecondPassphraseBadge: true,
 
-      hasUsername: false,
       isSecondPassphrase: false,
       isLocked: false,
 
       placeholder: '',
-      usernameForm: {
-        username: '',
-      },
-      usernameFormRules: {
-        username: [
-          {
-            required: true,
-            message: 'Please add a username',
-            trigger: 'blur',
-          },
-          {
-            type: 'string',
-            pattern: /^[a-z0-9_]{2,20}$/,
-            trigger: 'change',
-          },
-        ],
-      },
 
       secondPassphraseForm: {
         secondPassphrase: '',
@@ -337,26 +280,6 @@ export default {
     ]),
   },
   methods: {
-    async setUsername() {
-      try {
-        await this.$refs['usernameForm'].validate();
-      } catch (err) {
-        console.log('Validation for usernameForm failed');
-        return;
-      }
-
-      try {
-        const result = await connection.contract.Basic.setUserName(
-          this.usernameForm.username,
-          this.passphrase,
-          this.secondPassphrase,
-        );
-        this.$message(result.transactionId);
-        this.hasUsername = true;
-      } catch (err) {
-        console.log(err);
-      }
-    },
     async setSecondPassphrase() {
       try {
         await this.$refs['secondPassphraseForm'].validate();
