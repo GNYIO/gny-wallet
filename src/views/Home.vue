6455868e7f420e5167234cb9fc1502c339c8e46d
Matthias Knopp <matthias-knopp@gmx.net>
2020-06-27 15:00:16 +0200

    chore: add UnlockAccountComponent.vue #40

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,39 +26,13 @@
         v-bind:secondPassphrase="secondPassphrase"
       ></LockAccountComponent>
 
-      <el-col :span="12" v-if="user.lockHeight !== '0'">
-        <el-card v-if="!positiveBalance">
-          <div>
-            <h3>You need 0.1 GNY to unlock your account</h3>
-          </div>
-        </el-card>
-
-        <el-card v-if="positiveBalance">
-          <div slot="header">
-            Unlock your account
-          </div>
-
-          <el-form ref="unlockAccountForm" :model="unlockAccountForm">
-            <el-form-item>
-              <!--
-
-              -->
-              <el-badge
-                value="0.1 GNY"
-                type="info"
-                @mouseover.native="hideUnlockAccountBadge = false"
-                @mouseleave.native="hideUnlockAccountBadge = true"
-                :hidden="hideUnlockAccountBadge"
-              >
-                <!-- :disabled="alreadyDelegate"-->
-                <el-button type="primary" @click="unlockAccount"
-                  >Unlock Account</el-button
-                >
-              </el-badge>
-            </el-form-item>
-          </el-form>
-        </el-card>
-      </el-col>
+      <UnlockAccountComponent
+        v-bind:user="user"
+        v-bind:positiveBalance="positiveBalance"
+        v-bind:latestBlock="latestBlock"
+        v-bind:passphrase="passphrase"
+        v-bind:secondPassphrase="secondPassphrase"
+      ></UnlockAccountComponent>
     </el-row>
 
     <el-row :gutter="20">
@@ -76,16 +50,7 @@ import AccountInfoComponent from './Home/AccountInfoComponent';
 import SetUsernameComponent from './Home/SetUsernameComponent';
 import Set2ndPasswordComponent from './Home/Set2ndPasswordComponent';
 import LockAccountComponent from './Home/LockAccountComponent';
-
-import { BigNumber } from 'bignumber.js';
-
-import * as client from '@gny/client';
-const connection = new client.Connection(
-  process.env.VUE_APP_GNY_ENDPOINT,
-  Number(process.env.VUE_APP_GNY_PORT),
-  process.env.VUE_APP_GNY_NETWORK,
-  process.env.VUE_APP_HTTPS || false,
-);
+import UnlockAccountComponent from './Home/UnlockAccountComponent';
 
 export default {
   components: {
@@ -94,13 +59,11 @@ export default {
     SetUsernameComponent,
     Set2ndPasswordComponent,
     LockAccountComponent,
+    UnlockAccountComponent,
   },
   data() {
     return {
       placeholder: '',
-
-      unlockAccountForm: {},
-      hideUnlockAccountBadge: true,
     };
   },
   computed: {
@@ -111,35 +74,6 @@ export default {
       'hasSecondPassphrase',
       'minLockHeight',
     ]),
-  },
-  methods: {
-    async unlockAccount() {
-      const currentHeight = new BigNumber(this.latestBlock.height);
-      const lockHeight = new BigNumber(this.user.lockHeight);
-
-      if (currentHeight.isLessThan(lockHeight)) {
-        this.$message({
-          message: `You need to wait until height "${lockHeight.toFixed()}" to unlock your account`,
-          type: 'error',
-        });
-        return;
-      }
-
-      try {
-        const result = await connection.contract.Basic.unlockAccount(
-          this.passphrase,
-          this.secondPassphrase,
-        );
-        this.$message(result.transactionId);
-      } catch (err) {
-        const message =
-          err && err.response && err.response.data && err.response.data.error;
-        this.$message({
-          message: message,
-          type: 'error',
-        });
-      }
-    },
   },
   async mounted() {
     await this.$store.dispatch('refreshAccounts');
