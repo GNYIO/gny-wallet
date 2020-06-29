d10ad0dfd25848fdec44f40e620d5cc873af3de6
Matthias Knopp <matthias-knopp@gmx.net>
2020-06-29 06:13:07 +0200

    chore: add DelegateInfoComponent.vue

---
+++ src/views/Delegates/DelegateInfoComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-col :span="12">
+    <el-card v-if="user.isDelegate === 1">
+      <div slot="header">
+        <span>Delegate Info</span>
+      </div>
+
+      <el-row>
+        <el-col :span="12">
+          <p>
+            <span
+              >Delegate Name: <b>{{ user.username }}</b></span
+            >
+          </p>
+          <p>
+            <span
+              >Approval: <b>{{ delegate.approval }}</b></span
+            >
+          </p>
+          <p>
+            <span
+              >Fees (earned): <b>{{ delegate.fees }}</b></span
+            >
+          </p>
+          <p>
+            <span
+              >Rewards (earned): <b>{{ delegate.rewards }}</b></span
+            >
+          </p>
+          <p>
+            <span
+              >Produced Blocks: <b>{{ delegate.producedBlocks }}</b></span
+            >
+          </p>
+        </el-col>
+        <el-col :span="12">
+          <p>
+            <span
+              >Missed Blocks: <b>{{ delegate.missedBlocks }}</b></span
+            >
+          </p>
+          <p>
+            <span
+              >Rate (Position): <b>{{ delegate.rate }}</b></span
+            >
+          </p>
+          <p>
+            <span
+              >Votes: <b>{{ delegate.votes }}</b></span
+            >
+          </p>
+          <p>
+            <span
+              >Productivity: <b>{{ delegate.productivity }}</b></span
+            >
+          </p>
+        </el-col>
+      </el-row>
+    </el-card>
+  </el-col>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object,
+    delegate: Object,
+  },
+};
+</script>
--- src/views/Delegates/Index.vue
+++ src/views/Delegates/Index.vue
@@ -1,65 +1,10 @@
 <template>
   <div>
     <el-row :gutter="20">
-      <el-col :span="12">
-        <el-card v-if="user.isDelegate === 1">
-          <div slot="header">
-            <span>Delegate Info</span>
-          </div>
-
-          <el-row>
-            <el-col :span="12">
-              <p>
-                <span
-                  >Delegate Name: <b>{{ user.username }}</b></span
-                >
-              </p>
-              <p>
-                <span
-                  >Approval: <b>{{ delegate.approval }}</b></span
-                >
-              </p>
-              <p>
-                <span
-                  >Fees (earned): <b>{{ delegate.fees }}</b></span
-                >
-              </p>
-              <p>
-                <span
-                  >Rewards (earned): <b>{{ delegate.rewards }}</b></span
-                >
-              </p>
-              <p>
-                <span
-                  >Produced Blocks: <b>{{ delegate.producedBlocks }}</b></span
-                >
-              </p>
-            </el-col>
-            <el-col :span="12">
-              <p>
-                <span
-                  >Missed Blocks: <b>{{ delegate.missedBlocks }}</b></span
-                >
-              </p>
-              <p>
-                <span
-                  >Rate (Position): <b>{{ delegate.rate }}</b></span
-                >
-              </p>
-              <p>
-                <span
-                  >Votes: <b>{{ delegate.votes }}</b></span
-                >
-              </p>
-              <p>
-                <span
-                  >Productivity: <b>{{ delegate.productivity }}</b></span
-                >
-              </p>
-            </el-col>
-          </el-row>
-        </el-card>
-      </el-col>
+      <DelegateInfoComponent
+        v-bind:user="user"
+        v-bind:delegate="delegate"
+      ></DelegateInfoComponent>
 
       <el-col :span="12">
         <el-card v-if="user.isDelegate === 0 && user.username !== null">
@@ -302,6 +247,7 @@
 </template>
 
 <script>
+import DelegateInfoComponent from './DelegateInfoComponent';
 import DelegatesPagedComponent from './DelegatesPagedComponent';
 
 import { mapState, mapGetters } from 'vuex';
@@ -315,6 +261,7 @@ const connection = new client.Connection(
 
 export default {
   components: {
+    DelegateInfoComponent,
     DelegatesPagedComponent,
   },
   computed: {
