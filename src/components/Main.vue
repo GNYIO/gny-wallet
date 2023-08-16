a891faa14dc2b6996a9c672bf1f938952d93181b
Matthias Knopp <matthias-knopp@gmx.net>
2023-08-16 19:59:27 +0200

    chore: add /burn to left sidemenu

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -49,9 +49,9 @@
               <span> Assets</span>
             </el-menu-item>
 
-            <el-menu-item index="5" route="/machinelearning">
+            <el-menu-item index="5" route="/burn">
               <i class="el-icon-s-platform"></i>
-              <span> Machine Learning</span>
+              <span> Burn GNY</span>
             </el-menu-item>
 
             <el-menu-item index="6" route="/swapgate_eth">
