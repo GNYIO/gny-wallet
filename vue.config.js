e33eeaf105974d5eb71e77439d641ad35db414d4
a1300 <matthias-knopp@gmx.net>
2024-01-17 21:43:49 +0000

    chore: check env variables on startup

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
+const Web3 = require('web3');
+
 module.exports = {
   configureWebpack: {
     devtool: 'source-map',
   },
 };
+
+
+
+const bscChainId = 56;
+
+if (!Web3.utils.isAddress(process.env.VUE_APP_BSC_SWAPGATE_ADDRESS, bscChainId)) {
+  console.error('env variable VUE_APP_BSC_SWAPGATE_ADDRESS is not a valid address');
+  process.exit(1);
+}
+
+if (!Web3.utils.isAddress(process.env.VUE_APP_BSC_ERC20_ADDRESS, bscChainId)) {
+  console.error('env variable VUE_APP_BSC_ERC20_ADDRESS is not a valid address');
+  process.exit(1);
+}
+
+
+
+const ethChainId = 1;
+
